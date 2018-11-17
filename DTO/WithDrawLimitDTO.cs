22d32cce737ba154a71653004b29edf0efb6efcf
= <=>
2018-11-18 00:56:23 +0700

    Create DTO

---
+++ DTO/WithDrawLimitDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class WithDrawLimitDTO
+    {
+        private string wDID;
+        private decimal value;
+
+        public WithDrawLimitDTO()
+        {
+
+        }
+
+        public WithDrawLimitDTO(string wDID, decimal value)
+        {
+            this.wDID = wDID;
+            this.value = value;
+        }
+
+        public string WDID { get => wDID; set => wDID = value; }
+        public decimal Value { get => value; set => this.value = value; }
+    }
+}
