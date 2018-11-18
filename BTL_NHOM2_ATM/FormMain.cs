d85a95935f6607c764bc900621b0d60b4c680571
= <=>
2018-11-18 11:46:20 +0700

    Create form main

--- BTL_NHOM2_ATM/FormMain.cs
+++ BTL_NHOM2_ATM/FormMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BTL_NHOM2_ATM
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
