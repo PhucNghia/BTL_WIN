2539c1e6b0c3c1d27ba01b2624fa495297ae3894
Hau <hauhaui.k10@gmail.com>
2018-11-24 15:57:09 +0700

    add form Success

---
+++ GUI/Success.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Success : UserControl
+    {
+
+        private static Success _instance;
+
+        public Success()
+        {
+            InitializeComponent();
+        }
+
+        public static Success Instance
+        {
+            get
+            {
+                return _instance;
+            }
+
+            set
+            {
+                _instance = value;
+            }
+        }
+    }
+}
