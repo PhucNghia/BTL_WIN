c22c07f7cb5a5f7baff59712d28fbc63170814aa
Hau <hauhaui.k10@gmail.com>
2018-11-24 15:11:16 +0700

    add form Fail

---
+++ GUI/Fail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Fail : UserControl
+    {
+
+        private static Fail _instance;
+
+        public Fail()
+        {
+            InitializeComponent();
+        }
+
+        public static Fail Instance
+        {
+            get
+            {
+                return _instance;
+            }
+
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        public void showErrorMoney()
+        {
+            lbErrorMoney.Visible = true;
+            lbErrorCard.Visible = false;
+        }
+
+        public void showErrorCard()
+        {
+            lbErrorMoney.Visible = false;
+            lbErrorCard.Visible = true;
+        }
+    }
+}
